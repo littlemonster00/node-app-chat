473cb60191719f56c332d56d1e3f455b212655b6
sang le quang <sang.lequang97@gmail.com>
2018-07-20 10:03:05 +0700

    Add geolocation and use Geo API

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,19 @@ socket.on('connect', function () {
 		var li = jQuery('<li></li>');
 		li.text(`${message.from}: ${message.text}`);
 		jQuery('#messages').append(li);
-	});
+  });
+
+  socket.on('newLocationMessage', function (message) {
+		console.log('newMeassage', message);
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location<a/>');
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+		jQuery('#messages').append(li);
+  });
+  
+
 });
 socket.on('disconnect', function () {
 	console.log('disconnect from server');
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('gennerate message', () => {
   it('should generate correct message object', (done) => {
@@ -12,5 +12,17 @@ describe('gennerate message', () => {
      expect(message).toInclude({from, text});
      done();
   });
-
 });
+describe('generate location message', () => {
+  it('should be generate location message', (done) => {
+    var from = 'sang';
+    var laditude = 15;
+    var longitude = 18;
+    var url = 'https://www.google.com/maps?q=15,18';
+    var message = generateLocationMessage(from, laditude, longitude);
+    expect(message.url).toBe(url);
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
+    done();
+  })
+})
