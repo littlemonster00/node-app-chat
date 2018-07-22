9c2dba68ca58559794ca07b5df44251dcd5e4dd7
sang le quang <sang.lequang97@gmail.com>
2018-07-22 08:58:05 +0700

    remove all

--- playground/time.js
+++
@@ -1,7 +0,0 @@
-var moment = require('moment');
-
-var createAt = 1234;
-var someTimestamp = moment().valueOf();
-console.log(someTimestamp)
-var date = moment(createAt);
-console.log(date.format('h:mm a'));
